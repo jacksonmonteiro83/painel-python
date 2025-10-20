56028bba991f9166dbfae6e5a3a2881f3b02538e
jacksonmonteiro83 <61517181+jacksonmonteiro83@users.noreply.github.com>
2025-10-20 15:34:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import os
+import streamlit.web.cli as stcli
+import sys
 
-os.system("streamlit run painel.py --server.port=8501 --server.address=0.0.0.0")
+if __name__ == "__main__":
+    sys.argv = ["streamlit", "run", "painel.py", "--server.port=8501", "--server.address=0.0.0.0"]
+    sys.exit(stcli.main())
